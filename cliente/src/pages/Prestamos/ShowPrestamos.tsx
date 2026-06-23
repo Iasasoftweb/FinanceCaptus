486a5593f0bdd9fbf9d3a77726f8ac4c152470b6
Iasasoftweb <iasasoftweb@gmail.com>
2026-06-23 09:01:58 -0400

    Resolviendo el tema Ultimo Caso Extrao

--- cliente/src/pages/Prestamos/ShowPrestamos.tsx
+++ cliente/src/pages/Prestamos/ShowPrestamos.tsx
@@ -435,6 +435,8 @@ const ShowPrestamos = ({ situacion }) => {
       ? item?.situacion === "prestamo"
       : item?.situacion === "EVALUACION";
 
+      
+
   return (
     coincideZona &&
     coincideBusqueda &&
